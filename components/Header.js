d14c2a21065bbbb153aea4850e1a65ad64d21cb6
Werner Mona <wernermona96@gmail.com>
2022-06-02 16:57:43 +0200

    first route connected

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav-container">
+        <div className="nav-div">
+          <Link href="/">
+            <a className="nav-logo">The Drivers</a>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
