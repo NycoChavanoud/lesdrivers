72dfe758fd129faa0179abe24500c997aaa25d3c
GaspardMzt <gaspard.mzt@gmail.com>
2022-06-03 11:32:58 +0200

    remplissage basique des pages statiques

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,19 @@ export default function Header() {
         <Link href="/">
           <a className={HeaderStyle.link}>Home</a>
         </Link>
+
+        <Link href="/contact">
+          <a className={HeaderStyle.link}>Contact</a>
+        </Link>
+        <Link href="/faq">
+          <a className={HeaderStyle.link}>FAQ</a>
+        </Link>
+        <Link href="/nosservices">
+          <a className={HeaderStyle.link}>Nos Services</a>
+        </Link>
+        <Link href="/notresociete">
+          <a className={HeaderStyle.link}>Notre société</a>
+        </Link>
         <Link href="/articles">
           <a className={HeaderStyle.link}>Articles</a>
         </Link>
