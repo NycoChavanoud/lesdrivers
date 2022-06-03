46917f87865fca68548147ef001930dd6862bef5
GaspardMzt <gaspard.mzt@gmail.com>
2022-06-03 14:37:30 +0200

    favicon

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children, pageTitle }) {
       <Head>
         <title>{pageTitle}</title>
         <meta name="description" content="Welcome to the Driver Airport" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Header />
       {children}
