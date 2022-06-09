2513ea890f3d9f4b0f3b8eb45da7b77be0e640ea
GaspardMzt <gaspard.mzt@gmail.com>
2022-06-09 09:46:50 +0200

    modiffy the cypress test

--- cypress/integration/translation.spec.js
+++ cypress/integration/translation.spec.js
@@ -1,11 +1,11 @@
 describe("translation", () => {
   it("shows some content in fr", () => {
-    cy.visitInLanguage("/", "fr");
+    cy.visit("/");
     cy.contains("Accueil");
     //cy.get("[data-cy=bio]").should("be.visible");
   });
   it("has a translation in en", () => {
-    cy.visitInLanguage("/", "fr");
+    cy.visit("/");
     cy.get('[data-cy="translate-button"]').click();
     cy.contains("Home");
   });
