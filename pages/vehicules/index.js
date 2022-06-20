72084475f1a8df508e9154526afa1c8c3aaa9000
CLément <clmentmoreno@hotmail.com>
2022-06-20 15:40:37 +0200

    save

---
+++ pages/vehicules/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function TransfertAeroport() {
+  return (
+    <Layout pageTitle="Transfert aeroport">
+      <h1>Véhicules</h1>
+    </Layout>
+  );
+}
