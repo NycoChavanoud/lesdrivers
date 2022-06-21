3e142bc40ed4ded831e6f9db258c9bb31288410f
GaspardMzt <gaspard.mzt@gmail.com>
2022-06-21 15:57:52 +0200

    link with the deployed DBB

--- pages/transfert_aeroport/index.js
+++ pages/transfert_aeroport/index.js
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout";
 export default function TransfertAeroport() {
   return (
     <Layout pageTitle="Transfert aeroport">
-      <h1>Transfert Aeroport</h1>
+      <h1>Transfert vers aéroport</h1>
     </Layout>
   );
 }
