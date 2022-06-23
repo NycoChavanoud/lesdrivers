c26a159c7cd33e6631db988e350300e783fa2ec2
Werner Mona <wernermona96@gmail.com>
2022-06-23 10:11:41 +0200

    changes commited

--- pages/nos_services/index.js
+++ pages/nos_services/index.js
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout";
 export default function NosServices() {
   return (
     <Layout pageTitle="Les Drivers - Nos Services">
-      <h1>Nos Services</h1>
+      <h1>Qui sommes-nous ?</h1>
     </Layout>
   );
 }
