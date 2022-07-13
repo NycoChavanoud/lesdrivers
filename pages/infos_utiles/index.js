a3683785374e48a23a7054f6799fff517b9ec1a4
Werner Mona <wernermona96@gmail.com>
2022-07-13 14:25:06 +0200

    las commit

--- pages/infos_utiles/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../../components/Layout";
-
-export default function infos_utiles() {
-  return (
-    <Layout pageTitle="Les Drivers - Infos utiles">
-      <h1>Infos Utiles</h1>
-    </Layout>
-  );
-}
