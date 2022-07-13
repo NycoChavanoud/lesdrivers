bf77d0ecc305be62901cedf2df4eb55070c23cbf
Werner Mona <wernermona96@gmail.com>
2022-07-13 06:55:30 +0200

    pr validate

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,143 +1,66 @@
-import { useState } from "react";
-import axios from "axios";
-import toast, { Toaster } from "react-hot-toast";
+import React, { useRef } from "react";
+import Image from "next/image";
+import emailjs from "@emailjs/browser";
 import Layout from "../../components/Layout";
 import style from "../../styles/contact.module.css";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import emailPicture from "../../public/images/input_email.png";
 import profilePicture from "../../public/images/input_profil.png";
-import phonePicture from "../../public/images/input_phone.png";
-import adressPicture from "../../public/images/input_adress.png";
-import societyPicture from "../../public/images/input_society.png";
-const notifyRegisterSuccess = () => toast("Thanks ! You can now log in !");
+import email from "../../public/images/email.png";
+import message from "../../public/images/input_email.png";
 
-export default function SignupPage() {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [society, setSociety] = useState("");
-  const [error, setError] = useState("");
-  const router = useRouter();
+export default function ContactUs() {
+  const form = useRef();
 
-  const handleSubmit = (e) => {
-    setError("");
+  const sendEmail = (e) => {
     e.preventDefault();
-    axios
-      .post("/api/users", {
-        firstname,
-        email,
-        lastname,
-        message,
-        phoneNumber,
-      })
-      .then(() => router.push("/login"))
-      .then(notifyRegisterSuccess)
-      .catch((err) => {
-        if (err.response.status === 409) setError("This email already exists");
-      });
+
+    emailjs
+      .sendForm(
+        "service_hwrw8hn",
+        "template_oxvhj0n",
+        form.current,
+        "AUafpo6N2PVFopqgz"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
     <Layout pageTitle="Sign up">
-      <Toaster />
       <h1 className={style.mainTitle}>Contact :</h1>
-      <form className={style.signUpForm} onSubmit={handleSubmit}>
-        <div className={style.inputDivWith2InputsContainer}>
-          <div className={style.inputDivWith2Inputs}>
-            <label htmlFor="firstname">
+      <h3 className={style.mainText}>
+        Si vous avez des questions, envoyez-nous un message et nous vous
+        répondrons dans les plus brefs délais.
+      </h3>
+      <form ref={form} onSubmit={sendEmail} className={style.signUpForm}>
+        <div className={style.inputContainer}>
+          <div className={style.inputDiv}>
+            <label>
               <Image src={profilePicture} alt="Prénom" />
             </label>
-            <input
-              required
-              type="text"
-              id="firstname"
-              data-cy="firstname"
-              value={firstname}
-              placeholder="Prénom (*)"
-              onChange={(e) => setFirstname(e.target.value)}
-            />
+            <input type="text" name="user_name" />
           </div>
-          <div className={style.border} />
-          <div className={style.inputDivWith2Inputs}>
-            <label htmlFor="lastname"></label>
-            <input
-              required
-              type="text"
-              id="lastname"
-              data-cy="lastname"
-              value={lastname}
-              placeholder="Nom (*)"
-              onChange={(e) => setLastname(e.target.value)}
-            />
+          <div className={style.inputDiv}>
+            <label>
+              <Image src={email} alt="Prénom" />
+            </label>
+            <input type="email" name="user_email" />
+          </div>
+          <div className={style.inputDiv2}>
+            <label>
+              <Image src={message} alt="message" />
+            </label>
+            <textarea name="message" />
+          </div>
+          <div className={style.inputBtn}>
+            <input type="submit" value="Envoyer" className="btn" />
           </div>
         </div>
-
-        <div className={style.inputDiv}>
-          <label htmlFor="email">
-            <Image src={emailPicture} alt="Email" />
-          </label>
-          <input
-            required
-            type="email"
-            id="email"
-            data-cy="email"
-            value={email}
-            placeholder="Email (*)"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className={style.inputDiv}>
-          <label htmlFor="phoneNumber">
-            <Image src={phonePicture} alt="Phone number" />
-          </label>
-          <input
-            required
-            type="text"
-            id="phoneNumber"
-            data-cy="phoneNumber"
-            value={phoneNumber}
-            placeholder="Numéro de téléphone (*)"
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </div>
-
-        <div className={style.inputDiv}>
-          <label htmlFor="message">
-            <Image src={adressPicture} alt="message" />
-          </label>
-          <input
-            type="text"
-            id="message"
-            data-cy="message"
-            value={message}
-            minLength="50"
-            maxLength="500"
-            size="500"
-            placeholder="Message"
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
-
-        <div className={style.inputDiv}>
-          <label htmlFor="society">
-            <Image src={societyPicture} alt="Society" />
-          </label>
-          <input
-            type="text"
-            id="society"
-            data-cy="society"
-            value={society}
-            placeholder="Société"
-            onChange={(e) => setSociety(e.target.value)}
-          />
-        </div>
-        <p className={style.wrongDatas}>{error}</p>
-        <button data-cy="registerBtn" type="submit">
-          Envoyer
-        </button>
       </form>
     </Layout>
   );
